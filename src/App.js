0cda253361e120252af9bf784bc265620869b9db
Nataraj Boina <Nataraj.Boina@spacewell.com>
2020-06-21 00:14:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,184 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let navItems = [{
+  label: "Home"
+}, {
+  label: "Tutorials",
+  children: [{
+    label: "React"
+  }, {
+    label: "Angular"
+  }]
+}];
+
+let topNavItems = [{
+  label: "Home",
+  path: '/home'
+}, {
+  label: "Tutorials",
+  path: "/tutorials"
+}, {
+  label: "Careers",
+  path: "/careers"
+}, {
+  label: "About",
+  path: "/about"
+}];
+
+let cards = [{
+  title: "Card 1",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 1
+}, {
+  title: "Card 2",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 2
+}, {
+  title: "Card 3",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 3
+}, {
+  title: "Card 4",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 4
+}, {
+  title: "Card 5",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 5
+}, {
+  title: "Card 6",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 6
+}, {
+  title: "Card 6",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 7
+}, {
+  title: "Card 6",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 8
+}, {
+  title: "Card 6",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 9
+}, {
+  title: "Card 6",
+  description: "This is a demo card. A Square block to represent essentially any item",
+  id: 10
+}];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header />
+      <Container />
+    </>
+  );
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <div className="left-pane">
+        <List items={navItems} />
+      </div>
+      <div className="content">
+        <CardList items={cards} />
+      </div>
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header>
+      <div className="logo-container">
+        <img className="logo" src="logo192.png" alt="logo" />
+      </div>
+      <Navbar items={topNavItems} />
+    </header>
+  )
+}
+
+function ListItem(props) {
+  if (props.value.children && props.value.children.length > 0) {
+    return (
+      <li>
+        {props.value.label}
+        <List items={props.value.children} />
+      </li>
+    );
+  }
+  else {
+    return (
+      <li>
+        {props.value.label}
+      </li>
+    );
+  }
+}
+
+/**
+ * 
+ * @param {Array} items 
+ */
+function List(props) {
+  return (
+    <ul>
+      {
+        props.items.map((item, i) => {
+          return (
+            <ListItem key={i} value={item} />
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+function Navbar(props) {
+  return (
+    <nav>
+      {
+        props.items.map((item, index) => {
+          return (
+            <a href={item.path} className="nav-item" key={index}>
+              {item.label}
+            </a>
+          )
+        })
+      }
+    </nav>
+  )
+}
+
+function CardList(props) {
+  return (
+    <div className="card-layout">
+      {
+        props.items.map(item => {
+          return (
+            <Card key={item.id} title={item.title} description={item.description} />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="card-header">
+        {props.title}
+      </div>
+      <div className="card-body">
+        {props.description}
+      </div>
+    </div>
+  )
 }
 
 export default App;
